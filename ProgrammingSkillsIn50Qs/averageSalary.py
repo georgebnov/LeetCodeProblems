1d0bfabe8cf52ad013c487e7a9a5cb07ad2ace8d
George Babakhanov <gosha.babakhanov@gmail.com>
2025-09-03 14:08:37 -0400

    averageSalary.py - added a comment

--- ProgrammingSkillsIn50Qs/averageSalary.py
+++ ProgrammingSkillsIn50Qs/averageSalary.py
@@ -1,7 +1,7 @@
 salary1 = [4000,3000,1000,2000]
 salary = [48000,59000,99000,13000,78000,45000,31000,17000,39000,37000,93000,77000,33000,28000,4000,54000,67000,6000,1000,11000] #this one had issues with the floats. but thhats because it overflowed the bits.
 
-def average(salary):
+def average(salary):  #My Solution (0ms | Beats = 100%, 12.53MB | Beats 12.82%)
     avg = 0
     divisor = 0
 
@@ -29,3 +29,10 @@ def findMaxMin(salary):
     return max, min
 
 print(average(salary))
+
+
+'''
+What did I learn?
+1) apparently there is a differnec ebetween python3 and python. When I was dividing numbers, in python unless it was a float it floor divides it. so 10/3 = 3 but 10.0/3 = 3.3333
+2) Needs to not forget the self.function() that how you call it in class.
+'''
